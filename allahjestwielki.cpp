1fb37837b27d025dd16bbb0b64031a419996c0db
serek120 <155551160+serek120@users.noreply.github.com>
2025-01-30 12:13:17 +0100

    Add files via upload

---
+++ allahjestwielki.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstdio>
+#include "potrzebne.cpp"
+
+int main()
+{
+    game::time::DelayedPrinter printer(40);
+    printer.printWithDelay("witaj w grze `nadal jej nie nazwalem` jest to tekstowa gra RPG pisana na `kolanie`");
+    printer.printWithDelay("wpisz zacznij aby zaczac grac albo wylacz aby wylaczyc gre");
+
+    scanf_s("&c");
+
+    
+
+    return 0;
+}
+
