0c7229d1666e10eaa18df4e26254dbba8febfb2b
Maria <wojtalmarysia2007@gmail.com>
2025-02-06 12:15:16 +0100

    some changes

---
+++ txtgame/txtgame/klasyitd.cpp
@@ -0,0 +1,4 @@
+#include <string>
+#include <cstdlib>
+
+int klasa;
--- txtgame/txtgame/txtgame.cpp
+++ txtgame/txtgame/txtgame.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cstdio>
 #include "namespace.hpp"
+#include "klasyitd.cpp"
+
+char userinput;
 
 int main()
 {
     game::DelayedPrinter printer(5);
     printer.printWithDelay("witaj w grze \033[1;32;41m`nadal jej nie nazwalem`\033[0m jest to tekstowa gra RPG pisana na t. zw. \033[35m`kolanie`\033[0m");
+    
+    scanf_s("%c", userinput );
 
-    game::loading(2);
-
-    printer.printWithDelay("Wyberz klase");
-
-    printer.printWithDelay("Czlowiek\nLiso-czlowiek\n\nto tyle bo nie mam pomyslu na reszte");
+    game::loading(2);  
 
     return 0;
 }
